44606a82c501077a06cdec7dc31b054b8898244e
Andre <arc.xoc@gmail.com>
2022-05-11 02:30:37 -0700

    Assemblers

--- assembler.py
+++ assembler.py
@@ -2,6 +2,9 @@
 import os
 import sys
 
+# tell the user the script is starting
+print('Starting image assembler...')
+
 # check if the user is on linux
 if os.name != 'posix': 
     print('This script is only for linux')
@@ -25,11 +28,24 @@ if not os.path.isfile('raspbian.img'):
     print('Then place it in the same directory as this script')
     sys.exit(1)
 
+# check if custom-root.tar exists
+if not os.path.isfile('custom-root.tar'):
+    print('custom-root.tar not found')
+    sys.exit(1)
+
+# create a new raspbian image to modify
+print('creating new raspbian image...')
+os.system('sudo cp raspbian.img custom-raspbian.img')
+
 # run sudo losetup 
-os.system('sudo losetup -Pf raspbian.img')
+print('generating new loop device...')
+os.system('sudo losetup -Pf custom-raspbian.img')
+
+#change custom-raspbian to the same permissions
 
 # get the device name
-device = os.popen('losetup -j raspbian.img').read().strip()
+print('getting device name...')
+device = os.popen('losetup -j custom-raspbian.img').read().strip()
 
 # split the string to get the first part
 device = device.split(' ')[0]
@@ -50,15 +66,23 @@ import string
 random_hash = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
 print('Random hash: ' + random_hash)
 
+# re-assemble the image need to press y to confirm
+print('re-assembling image...')
+p = os.popen('sudo mkfs.ext4 /dev/' + device + 'p2', "w")
+
 # mount the device
+print('mounting root device...')
 os.system('sudo mkdir /mnt/' + random_hash)
 os.system('sudo mount /dev/' + device + 'p2 /mnt/' + random_hash)
 
-# create our root partition tar file
-os.system('sudo tar cf root.tar -C /mnt/' + random_hash + ' .')
+# copy the files to the new image
+print('copying files to new image...')
+os.system('sudo tar xf custom-root.tar -C /mnt/' + random_hash)
 
-# unmount the device
+#unmount the device
+print('unmounting root device...')
 os.system('sudo umount /mnt/' + random_hash)
 
-
-    
+# remove the losetup device
+print('removing loop device...')
+os.system('sudo losetup -d /dev/' + device)
